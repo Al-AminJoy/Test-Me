20d9f0a21e9de1b7992c8994fccf35960d8b7773
Al-AminJoy <alaminislam3555@gmail.com>
2024-01-06 13:14:19 +0600

    Toolbar brushup

--- app/src/main/java/com/alamin/testme/MainActivity.kt
+++ app/src/main/java/com/alamin/testme/MainActivity.kt
@@ -3,10 +3,20 @@ package com.alamin.testme
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Contrast
+import androidx.compose.material.icons.filled.Home
+import androidx.compose.material3.Button
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
@@ -15,36 +25,59 @@ import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarColors
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.alamin.testme.screen.HomeScreen
 import com.alamin.testme.ui.theme.Pink40
 import com.alamin.testme.ui.theme.TestMeTheme
+import com.alamin.testme.ui.theme.White
 
 class MainActivity : ComponentActivity() {
     @OptIn(ExperimentalMaterial3Api::class)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            TestMeTheme {
-                Scaffold(topBar = {
-                    TopAppBar(
-                        colors = TopAppBarDefaults.smallTopAppBarColors(containerColor = Color.Green, titleContentColor = Color.White),
-                        title = { Text(text = "Test Me") },
-
-                        )
-                }) {
-                    Box(modifier = Modifier.padding(it)) {
-                        App()
-                    }
-                }
-            }
+            App()
         }
     }
 
+    @OptIn(ExperimentalMaterial3Api::class)
     @Composable
     @Preview
     fun App() {
+
+        var themeMode = remember {mutableStateOf(true) }
+
+        TestMeTheme (darkTheme = themeMode.value){
+            Scaffold(topBar = {
+                TopAppBar(
+                    colors = TopAppBarDefaults.smallTopAppBarColors(containerColor = MaterialTheme.colorScheme.primary, titleContentColor = Color.White),
+                    title = { Row(horizontalArrangement = Arrangement.SpaceBetween, verticalAlignment = Alignment.CenterVertically, modifier = Modifier.fillMaxWidth()) {
+                        Text(text = "Test Me")
+                        IconButton(onClick = {
+                            themeMode.value = !themeMode.value
+                        }) {
+                            Icon(imageVector = Icons.Filled.Contrast, contentDescription = "Theme Changer")
+                        }
+                    }
+                    },
+
+                    )
+            }) {
+                Box(modifier = Modifier.padding(it)) {
+                   HomeScreen()
+                }
+            }
+        }
+
+
 
     }
 }
