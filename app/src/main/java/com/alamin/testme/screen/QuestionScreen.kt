91c3b49717eae6e8b79557c80ed0a13121f46b03
Al-AminJoy <alaminislam3555@gmail.com>
2024-01-17 22:17:36 +0600

    Question Text Design Added

--- app/src/main/java/com/alamin/testme/screen/QuestionScreen.kt
+++ app/src/main/java/com/alamin/testme/screen/QuestionScreen.kt
@@ -1,10 +1,10 @@
 package com.alamin.testme.screen
 
-import android.widget.Space
-import androidx.activity.compose.BackHandler
+import android.util.Log
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
@@ -13,31 +13,59 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.ElevatedButton
 import androidx.compose.material3.ElevatedCard
 import androidx.compose.material3.MaterialTheme
+
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.RectangleShape
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavHostController
 import com.alamin.testme.R
 import com.alamin.testme.model.data.Question
+import com.alamin.testme.ui.theme.White
 import com.alamin.testme.view_model.QuestionViewModel
 
+private const val TAG = "QuestionScreen"
 @Composable
 fun QuestionScreen(navController: NavHostController, questions: MutableList<Question>) {
 
     val questionViewModel: QuestionViewModel = hiltViewModel()
+    Log.d(TAG, "QuestionScreen: ")
 
     questionViewModel.setQuestionList(questions)
+
+/*    var newQuestionList by remember {
+        mutableStateOf(false)
+    }
+
+    LaunchedEffect(key1 = questions){
+        newQuestionList = true
+    }*/
+
+   // val question = questionViewModel.getQuestion()
+
+/*    if (newQuestionList){
+
+    }*/
 
     Column(
         verticalArrangement = Arrangement.SpaceBetween,
@@ -47,7 +75,7 @@ fun QuestionScreen(navController: NavHostController, questions: MutableList<Ques
             .background(MaterialTheme.colorScheme.background)
     ) {
         Text(
-            text = (questionViewModel.questionNo+1).toString(),
+            text = (questionViewModel.questionNo + 1).toString(),
             fontSize = 24.sp,
             fontWeight = FontWeight.Bold,
             color = MaterialTheme.colorScheme.primary,
@@ -57,7 +85,7 @@ fun QuestionScreen(navController: NavHostController, questions: MutableList<Ques
 
         Spacer(modifier = Modifier.height(8.dp))
 
-        ElevatedCard() {
+        ElevatedCard(colors = CardDefaults.cardColors(containerColor = MaterialTheme.colorScheme.primary)) {
             Column(
                 horizontalAlignment = Alignment.Start,
                 modifier = Modifier.padding(8.dp)
@@ -65,18 +93,46 @@ fun QuestionScreen(navController: NavHostController, questions: MutableList<Ques
                 Text(
                     text = questionViewModel.getQuestion().question,
                     fontSize = 14.sp,
-                    fontWeight = FontWeight.Bold
+                    fontWeight = FontWeight.Bold,
+                    modifier = Modifier
+                        .background(MaterialTheme.colorScheme.primary)
+                        .padding(8.dp)
+
                 )
                 Spacer(modifier = Modifier.height(8.dp))
-                LazyColumn(){
-                    items(questionViewModel.getQuestion().incorrectAnswers){
-                        Text(text = it)
+                /* val questions= (question.incorrectAnswers as MutableList)
+                 questions.add(question.correctAnswer)*/
+                val question = questionViewModel.getQuestion()
+                val questionSet = question.incorrectAnswers as MutableList
+               // questionSet.add(question.correctAnswer)
+                LazyColumn(contentPadding = PaddingValues(horizontal = 8.dp)) {
+
+                    items(questionSet) {
+                        Text(
+                            text = it,
+                            color = White,
+                            textAlign = TextAlign.Start,
+                            fontSize = 14.sp,
+                            modifier = Modifier
+                                .fillMaxWidth()
+                                .background(
+                                    MaterialTheme.colorScheme.tertiary,
+                                    shape = RoundedCornerShape(size = 8.dp)
+                                )
+                                .padding(8.dp)
+
+                        )
+                        Spacer(modifier = Modifier.height(4.dp))
                     }
                 }
             }
         }
 
-        Row (horizontalArrangement = Arrangement.SpaceBetween, verticalAlignment = Alignment.CenterVertically, modifier = Modifier.fillMaxWidth()){
+        Row(
+            horizontalArrangement = Arrangement.SpaceBetween,
+            verticalAlignment = Alignment.CenterVertically,
+            modifier = Modifier.fillMaxWidth()
+        ) {
             ElevatedButton(onClick = {
                 questionViewModel.decreaseQuestion()
             }) {
@@ -90,7 +146,7 @@ fun QuestionScreen(navController: NavHostController, questions: MutableList<Ques
         }
 
 
-
     }
+
 
 }
