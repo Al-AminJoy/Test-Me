57dd627b6a66b1576839fb47af209ba133a9e48b
Al-AminJoy <alaminislam3555@gmail.com>
2024-01-20 22:19:10 +0600

    Question toggle Added

--- app/src/main/java/com/alamin/testme/screen/QuestionScreen.kt
+++ app/src/main/java/com/alamin/testme/screen/QuestionScreen.kt
@@ -4,6 +4,7 @@ import android.util.Log
 import android.widget.Toast
 import androidx.activity.compose.BackHandler
 import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
@@ -41,6 +42,7 @@ import androidx.compose.ui.unit.sp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavHostController
 import com.alamin.testme.model.data.Question
+import com.alamin.testme.ui.theme.Green
 import com.alamin.testme.ui.theme.White
 import com.alamin.testme.view_model.QuestionViewModel
 
@@ -49,55 +51,49 @@ private const val TAG = "QuestionScreen"
 @Composable
 fun QuestionScreen(navController: NavHostController, questions: List<Question>) {
 
-    var isBackPressed by remember {
-        mutableStateOf(false)
-    }
+
     var isOpenDialog by remember {
         mutableStateOf(false)
     }
 
+
+
     BackHandler {
-        isBackPressed = true
         isOpenDialog = true
     }
 
-
-
-
-    if (isBackPressed) {
-        if (isOpenDialog) {
-            AlertDialog(
-                title = {
-                    Text(text = "Warning !")
-                }, text = { Text(text = "Do You Want to Cancel Exam ?") },
-                onDismissRequest = {
+    if (isOpenDialog) {
+        AlertDialog(
+            title = {
+                Text(text = "Warning !")
+            }, text = { Text(text = "Do You Want to Cancel Exam ?") },
+            onDismissRequest = {
+                isOpenDialog = false
+            }, confirmButton = {
+                Button(onClick = {
                     isOpenDialog = false
-                    isBackPressed = false
-                }, confirmButton = {
-                    Button(onClick = {
-                        isOpenDialog = false
-                        isBackPressed = false
-                        navController.popBackStack()
-                    }) {
-                        Text(text = "Confirm")
-                    }
+                    navController.popBackStack()
+                }) {
+                    Text(text = "Confirm")
+                }
 
-                },
-                dismissButton = {
-                    Button(onClick = {
-                        isOpenDialog = false
-                        isBackPressed = false
-                    }) {
-                        Text(text = "Dismiss")
-                    }
-                })
-        }
-
-
+            },
+            dismissButton = {
+                Button(onClick = {
+                    isOpenDialog = false
+                }) {
+                    Text(text = "Dismiss")
+                }
+            })
     }
+
 
     val questionViewModel: QuestionViewModel = hiltViewModel()
     Log.d(TAG, "QuestionScreen: ")
+
+    var nextQuestionIndex by remember {
+        mutableStateOf(questionViewModel.questionNo+1)
+    }
 
     ShowMessage(questionViewModel)
 
@@ -128,44 +124,8 @@ fun QuestionScreen(navController: NavHostController, questions: List<Question>) 
 
             Spacer(modifier = Modifier.height(8.dp))
 
-            ElevatedCard(colors = CardDefaults.cardColors(containerColor = MaterialTheme.colorScheme.primary)) {
-                Column(
-                    horizontalAlignment = Alignment.Start,
-                    modifier = Modifier.padding(8.dp)
-                ) {
-                    Text(
-                        text = questionViewModel.getQuestion(),
-                        fontSize = 14.sp,
-                        fontWeight = FontWeight.Bold,
-                        modifier = Modifier
-                            .background(MaterialTheme.colorScheme.primary)
-                            .padding(8.dp)
+            QuestionCard(questionViewModel)
 
-                    )
-                    Spacer(modifier = Modifier.height(8.dp))
-
-                    LazyColumn(contentPadding = PaddingValues(horizontal = 8.dp)) {
-
-                        items(questionViewModel.getAnswers()) {
-                            Text(
-                                text = it,
-                                color = White,
-                                textAlign = TextAlign.Start,
-                                fontSize = 14.sp,
-                                modifier = Modifier
-                                    .fillMaxWidth()
-                                    .background(
-                                        MaterialTheme.colorScheme.tertiary,
-                                        shape = RoundedCornerShape(size = 8.dp)
-                                    )
-                                    .padding(8.dp)
-
-                            )
-                            Spacer(modifier = Modifier.height(4.dp))
-                        }
-                    }
-                }
-            }
 
             Row(
                 horizontalArrangement = Arrangement.SpaceBetween,
@@ -179,6 +139,7 @@ fun QuestionScreen(navController: NavHostController, questions: List<Question>) 
                 }
                 ElevatedButton(onClick = {
                     questionViewModel.increaseQuestion()
+                    nextQuestionIndex = questionViewModel.questionNo
                 }) {
                     Text(text = "Next")
                 }
@@ -188,6 +149,61 @@ fun QuestionScreen(navController: NavHostController, questions: List<Question>) 
         }
     }
 
+
+}
+
+@Composable
+fun QuestionCard(questionViewModel: QuestionViewModel) {
+    ElevatedCard(colors = CardDefaults.cardColors(containerColor = MaterialTheme.colorScheme.primary)) {
+        Column(
+            horizontalAlignment = Alignment.Start,
+            modifier = Modifier.padding(8.dp)
+        ) {
+            Text(
+                text = questionViewModel.getQuestion(),
+                fontSize = 14.sp,
+                fontWeight = FontWeight.Bold,
+                modifier = Modifier
+                    .background(MaterialTheme.colorScheme.primary)
+                    .padding(8.dp)
+
+            )
+            Spacer(modifier = Modifier.height(8.dp))
+
+            var selectedOption by remember {
+                mutableStateOf("")
+            }
+            val onSelectionChange = { text: String ->
+                selectedOption = text
+            }
+
+            LazyColumn(contentPadding = PaddingValues(horizontal = 8.dp)) {
+
+                items(questionViewModel.getAnswers()) {
+
+                    Text(
+                        text = it,
+                        color = White,
+                        textAlign = TextAlign.Start,
+                        fontSize = 14.sp,
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .clickable {
+                                onSelectionChange(it)
+
+                            }
+                            .background(
+                                color = if (selectedOption == it) Green else MaterialTheme.colorScheme.tertiary,
+                                shape = RoundedCornerShape(size = 8.dp)
+                            )
+                            .padding(8.dp)
+
+                    )
+                    Spacer(modifier = Modifier.height(4.dp))
+                }
+            }
+        }
+    }
 
 }
 
--- app/src/main/java/com/alamin/testme/ui/theme/Color.kt
+++ app/src/main/java/com/alamin/testme/ui/theme/Color.kt
@@ -10,6 +10,10 @@ val Purple40 = Color(0xFF6650a4)
 val PurpleGrey40 = Color(0xFF625b71)
 val Pink40 = Color(0xFF7D5260)
 
+
 val White  = Color.White
 
 val Black = Color.Black
+
+val Green = Color(0xFF008000)
+
